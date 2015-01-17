dfc5e974f95eac9f3fe3a1cce5cd9625a502e013
Kevin "Schmidty" Smith <shenanigans@kaztl.com>
2015-01-17 11:38:12 -0800

    ready to publish

---
+++ main.js
@@ -0,0 +1,8 @@
+
+/**     @module infosex
+    **INFO**rmation **S**ciences **EX**change format package. Creates and parses standard strings
+    for a constellation of interoperable webapps.
+*/
+
+module.exports.uid      = require ('./lib/uid');
+module.exports.session  = require ('./lib/session');
